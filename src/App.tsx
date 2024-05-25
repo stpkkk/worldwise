f856726335cdf78837fa26d0707d8aacf83044a7
Igor Stepanov <stipyk1309@gmail.com>
2024-05-25 11:15:19 +0300

    feat: AppNav impl

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,22 @@ import {
 	Pricing,
 	Product,
 } from './pages'
+import { CityList } from './components'
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Homepage />} />
+				<Route index element={<Homepage />} />
 				<Route path='pricing' element={<Pricing />} />
 				<Route path='product' element={<Product />} />
 				<Route path='login' element={<Login />} />
-				<Route path='app' element={<AppLayout />} />
+				<Route path='app' element={<AppLayout />}>
+					<Route index element={<CityList />} />
+					<Route path='cities' element={<CityList />} />
+					<Route path='countries' element={<p>Country list</p>} />
+					<Route path='form' element={<p>Form</p>} />
+				</Route>
 				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
